baa28d7262a56ab5fec3b17b02c5ece07cc881f4
Taofeek Olusola <olusolataofeek@gmail.com>
2025-08-28 11:13:18 +0100

    latest update

---
+++ portfolio_project/src/data/comment.js
@@ -0,0 +1,8 @@
+// {
+//         "title": "Project F",
+//         "imageSrc": "projects/project.png",
+//         "description": "This is a responsive web project developed using HTML and CSS.",
+//         "skills": ["HTML", "CSS"],
+//         "demo": "https://responsive-techie-web.vercel.app/",
+//         "source": "https://github.com/Taofeekolusola/freecodecamp/tree/main/responsive_web"
+//     },
